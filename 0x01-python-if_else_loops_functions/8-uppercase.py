7cd8db58892acffd9dde9483069618aa3b1588ab
geno <yvesgeno@outlook.com>
2023-06-06 22:48:59 +0200

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(string):
     result = ""
     for char in string:
-        uppercase_char = chr(ord(char) - 32) if ord('a') <= ord(char) <= ord('z') else char
+        uppercase_char = chr(ord(char) & ~32) if 'a' <= char <= 'z' else char
         result += "{}".format(uppercase_char)
     print(result)
