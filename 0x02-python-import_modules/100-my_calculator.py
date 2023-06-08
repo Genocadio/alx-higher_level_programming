1b017b03ac574ea1f02c7563899d32b282b40e8a
geno <yvesgeno@outlook.com>
2023-06-08 20:13:33 +0200

    task1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    from calculator_1 import add, sub, mul, div
+    result = add(a, b)
+    diff = sub(a, b)
+    prod = mul(a, b)
+    divid = div(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    print("{} - {} = {}".format(a, b, diff))
+    print("{} * {} = {}".format(a, b, prod))
+    print("{} / {} = {}".format(a, b, divid))
+    
