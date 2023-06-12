b94d6923663d1a619df11937f2d200716216590d
geno <yvesgeno@outlook.com>
2023-06-12 23:24:32 +0200

    correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * is_palindrome - checks if a list is a palindrome
  * @head: head of the list
@@ -7,66 +6,43 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int count = countNode(*head);
-	int *arr = malloc(count * sizeof(int));
+	listint_t *slow = *head;
+	listint_t *fast = *head;
 
-	if (arr == NULL)
-		return (0);
-
-	assignToArray(*head, arr, count);
-
-	int i = 0;
-	int j = count - 1;
-
-	while (i < j)
+	while (fast != NULL && fast->next != NULL)
 	{
-		if (arr[i] != arr[j])
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+
+	listint_t *prev = NULL;
+
+	while (slow != NULL)
+	{
+		listint_t *next = slow->next;
+
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+
+	listint_t *p1 = *head;
+	listint_t *p2 = prev;
+
+	while (p1 != NULL && p2 != NULL)
+	{
+		if (p1->n != p2->n)
 		{
-			free(arr);
 			return (0);
 		}
-		i++;
-		j--;
+		p1 = p1->next;
+		p2 = p2->next;
 	}
 
-	free(arr);
 	return (1);
-}
-
-/**
- * countNode - counts the number of nodes in a linked list
- * @head: head of the list
- * Return: number of nodes in the list
- */
-int countNode(listint_t *head)
-{
-	int count = 0;
-	listint_t *current = head;
-
-	while (current != NULL)
-	{
-		count++;
-		current = current->next;
-	}
-	return (count);
-}
-
-/**
- * assignToArray - assign values of list to an array
- * @head: head of the list
- * @arr: array to assign values to
- * @size: size of the array
- * Return: void
- */
-void assignToArray(listint_t *head, int *arr, int size)
-{
-	listint_t *current = head;
-	int i = 0;
-
-	while (current != NULL && i < size)
-	{
-		arr[i] = current->n;
-		current = current->next;
-		i++;
-	}
 }
