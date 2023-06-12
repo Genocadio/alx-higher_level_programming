e2f2c1247e3fba399a1f7240fcf6fa373067580b
geno <yvesgeno@outlook.com>
2023-06-12 16:15:37 +0200

    correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = len(sentence)
-    b = sentence[0]
     if a == 0:
         b = None
+    else:
+        b = sentence[0]
     tup = (a, b)
     return tup
