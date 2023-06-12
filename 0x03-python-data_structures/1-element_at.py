3f7d775f534c30ac7fd1ee0a418c25bebe07adec
geno <yvesgeno@outlook.com>
2023-06-12 06:32:47 +0200

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     while idx < (len(my_list) - 1):
-        num = my_list[idx]
-        if num > 0:
-            print("Element at index {:d} is {:d}".format(idx, num))
+        if my_list[idx] > 0:
+            return my_list[idx]
         else:
             return None
     else:
