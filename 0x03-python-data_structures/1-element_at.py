f7a6ab7315059a10e1a212f14b1002179be2bbec
geno <yvesgeno@outlook.com>
2023-06-12 06:36:22 +0200

    correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     while idx < (len(my_list) - 1):
-        if my_list[idx] > 0:
+        if idx > 0:
             return my_list[idx]
         else:
             return None
