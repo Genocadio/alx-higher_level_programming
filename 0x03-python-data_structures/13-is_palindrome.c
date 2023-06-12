dab09c5f482b492f57f141a492b2ca065d07598c
geno <yvesgeno@outlook.com>
2023-06-13 00:16:00 +0200

    correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,76 +1,43 @@
 #include "lists.h"
-
 /**
- * countNode - counts the number of nodes in a linked list
- * @head: head of the list
- * Return: number of nodes in the list
- */
-int countNode(listint_t *head)
+ * reverse_listint - reverses a list
+ * @head: pointer to head of list
+ * Return: pointer to reversed list head
+*/
+listint_t *reverse_listint(listint_t **head)
 {
-	int count = 0;
-	listint_t *current = head;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
 	while (current != NULL)
 	{
-		count++;
-		current = current->next;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
-	return (count);
+
+	*head = prev;
+	return (*head);
 }
-
 /**
- * assignToArray - assign values of list to an array
- * @head: head of the list
- * @arr: array to assign values to
- * @size: size of the array
- * Return: void
- */
-void assignToArray(listint_t *head, int *arr, int size)
-{
-	listint_t *current = head;
-	int i = 0;
-
-	while (current != NULL && i < size)
-	{
-		arr[i] = current->n;
-		current = current->next;
-		i++;
-	}
-}
-
-/**
- * is_palindrome - checks if a list is a palindrome
- * @head: head of the list
- * Return: 1 if the list is a palindrome, 0 otherwise
- */
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 1 if palindrome, 0 else
+*/
 int is_palindrome(listint_t **head)
 {
-	int count = countNode(*head);
-	int *arr = malloc(count * sizeof(int));
+	listint_t *tmp = *head;
+	listint_t *reversed = reverse_listint(head);
 
-	assignToArray(*head, arr, count);
-
-	if (count % 2 == 1)
+	while (tmp != NULL && reversed != NULL)
 	{
-		int middle = count / 2;
-
-		free(arr[middle]);
-	}
-
-	int i = 0;
-	int j = count - 1;
-
-	while (i < j)
-	{
-		if (arr[i] != arr[j])
-		{
-			free(arr);
+		if (tmp->n != reversed->n)
 			return (0);
-		}
-		i++;
-		j--;
-	}
 
-	free(arr);
+		tmp = tmp->next;
+		reversed = reversed->next;
+	}
 	return (1);
 }
