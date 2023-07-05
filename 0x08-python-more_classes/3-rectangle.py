e8d649bc5b62dbc3ab743ce14feec79d4b5032a9
geno <yvesgeno@outlook.com>
2023-07-05 21:13:26 +0200

    correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,9 +52,6 @@ class Rectangle:
         """Returns the printable representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        rect = ['#'] * self.__width + ['\n']
+        rect = rect * (self.__height - 1) + ['#'] * self.__width
+        return ''.join(rect)
