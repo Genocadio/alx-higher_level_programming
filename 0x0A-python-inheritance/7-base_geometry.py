0d6ec371642799fceda24f78b0b234ad28e0fe02
geno <yvesgeno@outlook.com>
2023-07-12 22:46:17 +0200

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """function area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
