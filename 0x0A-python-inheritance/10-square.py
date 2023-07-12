f1a54ca7b883a563172e6539fe9abb9696da89d9
geno <yvesgeno@outlook.com>
2023-07-12 23:13:34 +0200

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""impout rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+    def __init__(self, size):
+        """Initialize Square"""
+        super().__init__(size, size)
+
+    def area(self):
+        """function area"""
+        return self.__size * self.__size
