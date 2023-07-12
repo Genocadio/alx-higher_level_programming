ca20296c9ebf8cdc7fce91d09c10ec557f5f8510
geno <yvesgeno@outlook.com>
2023-07-12 21:57:12 +0200

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# """module for is_same_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of class, else False"""
+    return isinstance(obj, a_class)
