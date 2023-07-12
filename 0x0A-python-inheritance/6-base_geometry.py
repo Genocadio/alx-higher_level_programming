94f62427207f78bdbf1ee19b8faeb94510924e87
geno <yvesgeno@outlook.com>
2023-07-12 22:18:13 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """function area"""
+        raise Exception("area() is not implemented")
