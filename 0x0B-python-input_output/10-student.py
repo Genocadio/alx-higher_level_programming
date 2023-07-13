02c940a08a40e3686ae03f10489b54400a9aa8fa
Geno <yvesgeno@outlook.com>
2023-07-14 00:06:40 +0200

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module is for class Student"""
+
+
+class Student:
+    """This is the class Student"""
+    def __init__(self, first_name, last_name, age):
+        """This is the init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This is the method to_json"""
+        if type(attrs) == list:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
