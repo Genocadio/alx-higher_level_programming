65a513f5b6d4bcae1bd45cc14c44bbc592173bf3
Geno <yvesgeno@outlook.com>
2023-07-13 23:48:13 +0200

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""import json"""
+import json
+
+
+def class_to_json(obj):
+    """This function returns the JSON representation of an object (string)"""
+    return json.dumps(obj)
