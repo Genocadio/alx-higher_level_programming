26b008c926cde54e1b24803989dbcb69dce07552
Geno <yvesgeno@outlook.com>
2023-07-13 23:05:29 +0200

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """This function writes a text file (UTF8)"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
