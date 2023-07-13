72d241403a603cdaaf1f8c4d86f590ec80c475de
Geno <yvesgeno@outlook.com>
2023-07-13 23:18:52 +0200

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""import json"""
+import json
+
+
+def from_json_string(my_str):
+    """This function returns the JSON representation of an object (string)"""
+    return json.loads(my_str)
