52ffbc0ccab2097e50906acb95952fabf0525502
Geno <yvesgeno@outlook.com>
2023-07-13 22:54:19 +0200

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# """This module writes a text file (UTF8)"""
+
+
+def write_file(filename="", text=""):
+    """This function writes a text file (UTF8)"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
