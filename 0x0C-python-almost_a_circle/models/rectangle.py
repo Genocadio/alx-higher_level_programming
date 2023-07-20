683e177895af76db0b97230ff01dd2c94d9cce66
Geno <yvesgeno@outlook.com>
2023-07-20 23:00:06 +0200

    correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,10 +7,7 @@ from base import Base
 class Rectangle(Base):
     """Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Initializes the class Rectangle
-        it inherits from Base
-        """
+        """Initializes the class Rectangle"""
         super().__init__(id)
         """class initialization"""
         self.width = width
@@ -20,18 +17,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-        width getter
-        This will return the width
-        """
+        """width getter, This will return the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        width setter
-        This will set the width and check if width is int
-        """
+        """width setter"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -40,18 +31,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """
-        height getter
-        This will return the height
-        """
+        """height getter, This will return the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        height setter
-        This will set the height and check if height is int
-        """
+        """height setter, This will set the height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -60,18 +45,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """
-        x getter
-        This will return the x
-        """
+        """x getter, This will return the x"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
-        x setter
-        This will set the x and check if x is int
-        """
+        """x setter, This will set the x and check if x is int"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -80,18 +59,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """
-        y getter
-        This will return the y
-        """
+        """y getter, This will return the y"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        y setter
-        This will set the y and check if y is int
-        """
+        """y setter, This will set the y and check if y is int"""
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -99,38 +72,24 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        """
-        This uses width and height to calculate the area
-        Returns the area of the rectangle
-        """
+        """Returns the area of the rectangle"""
         return self.__width * self.__height
 
     def display(self):
-        """
-        This will print the rectangle
-        Prints the rectangle with the character #
-        """
+        """Prints the rectangle with the character #"""
         for i in range(self.__y):
             print()
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        """
-        This converts the rectangle to a string
-        Returns the string representation of the rectangle
-        """
+        """Returns the string representation of the rectangle"""
         hei = self.height
         r = f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{hei}"
         return r
 
     def update(self, *args, **kwargs):
-        """
-        alguments:
-        args: list of attributes to set
-        kwargs: dictionary of attributes to set
-        Updates the public instance attributes
-        """
+        """Updates the public instance attributes"""
         if len(args) > 0:
             if len(args) > 0:
                 self.id = args[0]
@@ -155,10 +114,7 @@ class Rectangle(Base):
                 self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """
-        It converts the rectangle to a dictionary
-        Returns the dictionary representation of the rectangle
-        """
+        """Returns the dictionary representation of the rectangle"""
         return {
             "id": self.id,
             "width": self.width,
