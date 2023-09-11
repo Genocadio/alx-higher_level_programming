3b93df339140ccd4a7edaafdb91c067f2590b5c0
geno <yvesgeno@outlook.com>
2023-09-12 00:53:30 +0200

    correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-function secondBiggestInteger () {
-  const args = Array.from(arguments).map(Number);
-  const uniqueArgs = [...new Set(args)];
-  const sortedArgs = uniqueArgs.sort((a, b) => b - a);
-  return sortedArgs[1] || 0;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-console.log(secondBiggestInteger(1, 2, 3, 4, 5)); // 4
-console.log(secondBiggestInteger(1, 2)); // 1
-console.log(secondBiggestInteger()); // 0
