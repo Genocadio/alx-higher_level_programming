e9d9202fb66a027a0162633bf020a4b67e1917e5
geno <yvesgeno@outlook.com>
2023-09-13 23:38:47 +0200

    task

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constuructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
-}
+  constuructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
