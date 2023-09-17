4c68834f4bc1331d70e8c787e44c332aa59f37e5
geno <yvesgeno@outlook.com>
2023-09-17 22:46:32 +0200

    task3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module that lists all states from the database"""
+import MySQLdb
+from sys import argv
+
+
+def filter_states(username, password, dbname, stte):
+    """function that lists all states from the database"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=dbname)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [stte])
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()
+    
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+    stte = argv[4]
+    filter_states(username, password, dbname, stte)
