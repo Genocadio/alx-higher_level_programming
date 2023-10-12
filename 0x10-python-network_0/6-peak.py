2f63331549229d7ee304d2a3866f8de3a24aa610
geno <yvesgeno@outlook.com>
2023-10-12 10:37:44 +0200

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """"finds a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return
+    li = list_of_integers
+    length = len(li)
+    if length == 0:
+        return
+    mid = length // 2
+    if (mid == length - 1 or li[mid] >= li[mid + 1]) and\
+       (mid == 0 or li[mid] >= li[mid - 1]):
+        return li[mid]
+    elif mid != length - 1 and li[mid + 1] > li[mid]:
+        return find_peak(li[mid + 1:])
+    return find_peak(li[:mid])
